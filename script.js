1975a3d4af97b8c2e32ba2a97fc0b8cd61175a35
Konstantin-2K <koseto2k@gmail.com>
2023-03-14 18:10:10 +0200

    HAVE TO CALCULATE RESULT BEFORE PRINTING IT TO DISPLAY AGAIN WITH NEW OPERATOR

--- script.js
+++ script.js
@@ -1,76 +1,127 @@
 let display = document.querySelector('.display');
+display.textContent = 0;
 let result = document.querySelector('.result');
 
-let inputButtons = document.querySelectorAll('.input');
-
-for (let button of inputButtons) {
-    button.addEventListener('click', draw);
-}
-
-
+let numButtons = document.querySelectorAll('.input');
 let funcButtons = document.querySelectorAll('.func');
+let clearBtn = document.querySelector('.clear');
+let deleteBtn = document.querySelector('.delete');
+let equalsBtn = document.querySelector('.equals');
+
+for (let button of numButtons) {
+    button.addEventListener('click', displayDraw);
+}
 
 for (let button of funcButtons) {
-    button.addEventListener('click', operate);
+    button.addEventListener('click', appendFunc);
 }
 
-
-function draw() {
-    result.textContent += this.textContent;
+clearBtn.onclick = () => {
+    display.textContent = 0;
+    result.textContent = '';
+    firstNum = '';
+    secondNum = '';
+    currentOperation = null;
+    resultNum = '';
 }
 
-let firstNum = 0;
-let secondNum = 0;
-
-function operate() {
-    switch (this.textContent) {
-        case "AC":
-            clear();
-            break;
-
-        case "%":
-            percentage();
-            break;
-
-        case "C":
-            backspace();
-            break;
-
-        case "/":
-            divide();
-            break;
-
-        case "*":
-            multiply();
-            break;
-
-        case "-":
-            minus();
-            break;
-
-        case "+":
-            firstNum = result.textContent;
-            display.textContent = result.textContent;
-            result.textContent = '';
-
-            plus(firstNum, secondNum);
-            break;
-
-        case "=":
-
-            break;
+deleteBtn.onclick = () => {
+    if (display.textContent.length === 1) {
+        display.textContent = 0;
+    } else if (display.textContent.length !== '0') {
+        display.textContent = display.textContent.substring(0, display.textContent.length - 1);
     }
 }
 
-function plus(a, b) {
+equalsBtn.onclick = () => {
+    if (display.textContent !== '0') {
+        evaluate();
+    }
+}
+
+function displayDraw() {
+    if (display.textContent === '0') {
+        display.textContent = '';
+    }
+    display.textContent += this.textContent;
+}
+
+const functions = ['+', '-', '*', '/', '%'];
+let firstNum = '';
+let secondNum = '';
+let currentOperation = null;
+let resultNum = '';
+let expression = [];
+
+function evaluate() {
+    let expression = display.textContent.split(currentOperation);
+    firstNum = Number(expression[0]);
+    secondNum = Number(expression[1]);
+    if (secondNum === 0 && currentOperation === '/') {
+        result.textContent = "Error!";
+    } else {
+        result.textContent = roundResult(operate(currentOperation, firstNum, secondNum));
+    }
+
+}
+
+function roundResult(num) {
+    return Math.round(num * 1000) / 1000;
+}
+
+function appendFunc() {
+    switch (this.textContent) {
+        case "+":
+            currentOperation = '+';
+            break;
+        case "-":
+            currentOperation = '-';
+            break;
+        case "*":
+            currentOperation = '*';
+            break;
+        case "/":
+            currentOperation = '/';
+            break;
+        case "%":
+            currentOperation = '%';
+            break;
+    }
+    if (!functions.includes(display.textContent.charAt(display.textContent.length - 1)) && result.textContent === '') {
+        display.textContent += currentOperation;
+        evaluate();
+    } if (result.textContent !== '') {
+        display.textContent = `${result.textContent}${currentOperation}`;
+        result.textContent = '';
+        appendFunc();
+    }
+}
+
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return substract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) return null;
+            else return divide(a, b);
+        case '%':
+            return percent(a, b)
+        default:
+            return null
+    }
+}
+
+function add(a, b) {
     return a + b;
 }
 
-function percentage(a, b) {
-    return (b * a) / 100;
-}
-
-function minus(a, b) {
+function substract(a, b) {
     return a - b;
 }
 
@@ -82,18 +133,14 @@ function divide(a, b) {
     return a / b;
 }
 
-function clear() {
-    display.textContent = '';
-    result.textContent = '';
+function percent(a, b) {
+    return b * a / 100;
 }
 
-function backspace() {
-    display.textContent = display.textContent.substring(0, display.textContent.length - 1);
-}
-
-/*setInterval(logger, 2000);
-
+setInterval(logger, 2000);
 function logger() {
-    console.log(input);
     console.log(firstNum);
-}*/
+    console.log(secondNum);
+    console.log(currentOperation);
+    console.log(resultNum);
+}
